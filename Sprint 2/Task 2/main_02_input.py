bf42a5cbf703e2841214b9d9b31a19bccc9dbf08
123qwertyuiopas <pimenoveu12@gmail.com>
2022-06-07 15:13:27 +0300

    Discription

--- Sprint 2/Task 2/main_02_input.py
+++ Sprint 2/Task 2/main_02_input.py
@@ -1,4 +1,4 @@
-with open(f'{input("Путь к файлу:")}', 'rb') as f:
+with open(f'{input("Путь к файлу: ")}', 'rb') as f:
     symbols = f.read(2)
 if hex(symbols[0] * 2 ** 8 + symbols[1])[2:] == '4d5a':
     print(f'{hex(symbols[0] * 2 ** 8 + symbols[1])[2:]} {symbols.decode("utf-8")} - executable, OS Windows')
